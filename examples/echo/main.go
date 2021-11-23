4cb326a06cebcb775ac06bc4c8bad7c1460904db
Aleksandr Razumov <ernado@ya.ru>
2021-11-23 19:20:15 +0300

    feat(echo): add args

--- examples/echo/main.go
+++ examples/echo/main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 
 	"github.com/go-faster/hx"
 )
 
 func main() {
+	var arg struct {
+		Workers int
+		Addr    string
+	}
+	flag.StringVar(&arg.Addr, "addr", "localhost:8080", "listen address")
+	flag.IntVar(&arg.Workers, "j", 500, "count of workers")
+	flag.Parse()
 	s := &hx.Server{
 		Workers: 500,
 		Handler: func(ctx *hx.Ctx) {
 			ctx.Response.AppendBodyString("Hello, world")
 		},
 	}
-	log.Fatal(s.ListenAndServe(context.Background(), "localhost:8080"))
+	log.Fatal(s.ListenAndServe(context.Background(), arg.Addr))
 }
